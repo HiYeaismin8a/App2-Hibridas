9005763459b1cf37b7685355e9fc718f110ec9c5
Jazmin Ochoa Benitez <jazmin_2303@hotmail.com>
2022-10-14 07:02:18 -0600

    compilacion y despliegue

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -6,7 +6,30 @@ import { Component } from '@angular/core';
   styleUrls: ['home.page.scss'],
 })
 export class HomePage {
+  public photos = [];
 
-  constructor() {}
+
+  constructor() {
+
+    this.photos = [
+      {
+        name: "Foto 1",
+        descripcion: "Lorem ipsum dolor sit amet consectetur adipisicing elit.",
+        url: "https://picsum.photos/id/237/200/300"
+      },
+      {
+
+          name: "Foto 2",
+          descripcion: "Lorem ipsum dolor sit amet consectetur adipisicing elit.",
+          url: "https://picsum.photos/id/238/200/300"
+
+      },
+      {
+        name: "Foto 3",
+        descripcion: "Lorem ipsum dolor sit amet consectetur adipisicing elit.",
+        url: "https://picsum.photos/id/239/200/300"
+      }
+    ]
+  }
 
 }
